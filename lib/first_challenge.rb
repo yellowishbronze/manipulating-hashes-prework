45e82b3f09f5fb66eb9a450061f166dac1306273
Bryan Teng <bteng1226@gmail.com>
2018-06-26 01:13:33 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,9 +15,11 @@ def first_challenge
 
   #your code here
   index = 0
-  while index < 
-contacts["Freddy Mercury"][favorite_icecream_flavors]
-first_challenge["Freddy Mercury"][:favorite_icecream_flavors][0]
+  while index < contacts["Freddy Mercury"][favorite_icecream_flavors].length
+    flavors = first_challenge["Freddy Mercury"][:favorite_icecream_flavors]
+    if flavors[index] == "strawberry"
+    index+=1
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
